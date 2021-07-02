85104ff97afe5f4154f1af0215e45d1e37b03a81
hcthcm290 <dragonnica123@gmail.com>
2021-07-02 20:51:50 +0700

    Can edit profile now

--- lib/Screens/UserInfoScreen/EditProfileScreen.dart
+++ lib/Screens/UserInfoScreen/EditProfileScreen.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Model/UserModel.dart';
 import 'package:flutter_application_1/Screens/DetailPostScreen/DetailPostScreen.dart';
+import 'package:flutter_application_1/Services/UserCredentialService.dart';
 import 'package:flutter_application_1/constant.dart';
 
 class EditProfileScreen extends StatefulWidget {
@@ -20,6 +23,9 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
   ImageProvider _avatarImage = null;
   File _fileAvatar;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController descController = TextEditingController();
 
   Future<void> _getAvatarImage() async {
     if (widget.userModel.avatarUrl == null ||
@@ -40,10 +46,60 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 setState(() {
                   _fileAvatar = image;
                   _avatarImage = Image.file(image).image;
+                  _infoChanged = true;
                 });
                 Navigator.pop(context);
               },
             ));
+  }
+
+  Future<void> saveInfo() async {
+    if (!_infoChanged) return;
+
+    var result = showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+
+    var userDocRef = FirebaseFirestore.instance
+        .collection("users")
+        .doc(UserCredentialService.instance.currentUser.uid);
+
+    if (_fileAvatar != null) {
+      var imageData = await _fileAvatar.readAsBytes();
+      var image = await FirebaseStorage.instance
+          .ref("users")
+          .child(widget.userModel.id)
+          .child("image.png")
+          .putData(imageData);
+
+      var url = await image.ref.getDownloadURL();
+
+      Map<String, dynamic> avatarMap = {};
+      avatarMap["avatarUrl"] = url;
+
+      await userDocRef.update(avatarMap);
+
+      _fileAvatar = null;
+      this.widget.userModel.avatarUrl = url;
+    }
+
+    Map<String, dynamic> infos = {};
+    infos["displayName"] = nameController.text;
+    infos["description"] = descController.text;
+    await userDocRef.update(infos);
+
+    this.widget.userModel.displayName = nameController.text;
+    this.widget.userModel.description = descController.text;
+
+    Navigator.of(context, rootNavigator: true).pop();
+
+    _infoChanged = false;
+    setState(() {});
   }
 
   @override
@@ -51,6 +107,25 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     // TODO: implement initState
     super.initState();
     _getAvatarImage();
+    nameController.text = widget.userModel.displayName;
+    nameController.addListener(() {
+      if (this._infoChanged == false &&
+          nameController.text != widget.userModel.displayName) {
+        this._infoChanged = true;
+        setState(() {});
+      }
+    });
+
+    descController.text = widget.userModel.description != null
+        ? "${widget.userModel.description}"
+        : "My Funny Collection";
+    descController.addListener(() {
+      if (this._infoChanged == false &&
+          nameController.text != widget.userModel.description) {
+        this._infoChanged = true;
+        setState(() {});
+      }
+    });
   }
 
   @override
@@ -76,7 +151,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             ),
             Spacer(),
             TextButton(
-              onPressed: () {},
+              onPressed: saveInfo,
               child: Text("Save",
                   style: Theme.of(context).textTheme.bodyText1.copyWith(
                       color: _infoChanged ? Colors.blue : Colors.white54,
@@ -131,7 +206,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 ),
                 Flexible(
                   child: TextFormField(
-                    initialValue: "${widget.userModel.displayName}",
+                    controller: nameController,
                     textAlign: TextAlign.end,
                     style: Theme.of(context)
                         .textTheme
@@ -163,9 +238,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                           .copyWith(color: Colors.white, fontSize: 12),
                     ),
                     TextFormField(
-                      initialValue: widget.userModel.description != null
-                          ? "${widget.userModel.description}"
-                          : "My Funny Collection",
+                      controller: descController,
                       textAlign: TextAlign.start,
                       maxLines: 3,
                       minLines: 1,
